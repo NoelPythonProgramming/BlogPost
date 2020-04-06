70d72e14fd8e30c6c7f7963224553446da61f2ec
NoelPythonProgramming <63259735+NoelPythonProgramming@users.noreply.github.com>
2020-04-06 18:02:32 -0400

    Added __str__

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -5,3 +5,6 @@ class BlogPost(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
