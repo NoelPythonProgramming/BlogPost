35581c5280939767f56d2535f37200a9cccfa0c3
NoelPythonProgramming <63259735+NoelPythonProgramming@users.noreply.github.com>
2020-04-06 19:39:32 -0400

    Added 'topics()' renders topics.html

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-from .models import BlogPost
+from .models import Topic
 # Create your views here.
 
 def index(request):
 	return render(request, 'blog_posts/index.html')
+
+def topics(request):
+	topics = Topic.objects.all()
+	context = {'topics': topics}
+	return render(request, 'blog_posts/topics.html', context)
