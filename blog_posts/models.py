4a5092fcda913cb014f16c59f4b4a04b9f0ee726
NoelPythonProgramming <63259735+NoelPythonProgramming@users.noreply.github.com>
2020-04-06 19:40:15 -0400

    Delete

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class BlogPost(models.Model):
-	title = models.CharField(max_length=200)
-	text = models.TextField()
+class Topic(models.Model):
+	topic = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return self.title
+		return self.topic
+
+class Entry(models.Model):
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'Entries'
+
+	def __str__(self):
+		return f"{self.text[:50]}..."
